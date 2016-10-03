d64282af96358e2958eef074f344d2e8e0d73259
Nick Oki <nick.oki@live.com>
2016-10-03 16:42:36 -0400

    Refactored with layout.erb, styles, <%= yield %>

--- app.rb
+++ app.rb
@@ -1,2 +1,47 @@
 require 'sinatra'
 require 'sinatra/reloader'
+
+get '/' do
+	@text = "Do you have a test for that?"
+	@yes_link = '/write_test'
+	@no_link = '/pass'
+	erb :question
+end
+
+get '/write_test' do
+	@text = "Write a test."
+	@done_link = '/pass'
+	erb :instruction
+end
+
+get '/pass' do
+	@text = "Does the test pass?"
+	@yes_link = '/refactor'
+	@no_link = '/write_code'
+	erb :question
+end
+
+get '/write_code' do
+	@text = "Write just enough code for the test to pass."
+	@done_link = '/pass'
+	erb :instruction
+end
+
+get '/refactor' do
+	@text = "Need to refactor?"
+	@yes_link = '/do_refactor'
+	@no_link = '/new_feature'
+	erb :question
+end
+
+get '/do_refactor' do
+	@text = "Refactor the code."
+	@done_link = '/pass'
+	erb :instruction
+end
+
+get '/new_feature' do
+	@text = "Select a new feature to implement."
+	@done_link = '/'
+	erb :instruction
+end
